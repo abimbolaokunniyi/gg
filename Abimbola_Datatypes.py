493da69bb814269f95f8eddbaf4ade4f94d82e29
abimbolaokunniyi <okunniyiabimbola@gmail.com>
2025-03-21 02:45:23 +0100

    Add files via upload

---
+++ Abimbola_Datatypes.py
@@ -0,0 +1,27 @@
+print(type (30))
+print(type(-45))
+print(type(17.3))
+print(type("how are you"))
+print(type(True))
+print(type(3<5))
+print(int(45+60))
+print(float(99.5*100))
+
+
+x=16
+y=float(x)
+print(y)
+
+k=20.55
+p=int(k)
+print(p)
+
+name="victor"
+age="13"
+print ("my nephew is named "+name+" ")
+print ("he is "+age+"")
+
+Car_brands =['Toyota', 'Honda', 'Ferrari', 'Mercedes Benz']
+Car_brands.append('Nissan')
+Car_brands[0]=('Lexus')
+print(Car_brands)
